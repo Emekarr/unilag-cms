2d57421ef393e09ab29c412ab4fc6fcaa1d3a447
Emekarr <aceanyanwu02@gmail.com>
2021-10-19 22:01:44 +0100

    set up routing system

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Application, Request, Response } from "express";
 
+import router from "./routes/index.js";
+
 class App {
   private express: Application;
   constructor() {
     this.express = express();
+
+    this.express.use("/api", router);
 
     this.express.get("/health", (req: Request, res: Response) => {
       res.send("server is alive");
