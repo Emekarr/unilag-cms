359b487255fac4006bc78d718d14a9e98463a971
Emekarr <aceanyanwu02@gmail.com>
2021-10-19 21:57:15 +0100

    add ping endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 
 class App {
   private express: Application;
   constructor() {
     this.express = express();
+
+    this.express.get("/health", (req: Request, res: Response) => {
+      res.send("server is alive");
+    });
   }
 
   listen(port: string, cb: () => void) {
