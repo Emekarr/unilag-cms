10b77957379d50c50a10a168943b50f54131fa66
Emekarr <aceanyanwu02@gmail.com>
2021-10-20 16:22:08 +0100

    built student model

---
+++ src/model/student.ts
@@ -0,0 +1,115 @@
+import { Schema, model, Document, HookNextFunction } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+export interface IStudent {
+  firstname: string;
+  lastname: string;
+  username: string | null;
+  department: string;
+  matric_no: string;
+  phone: string;
+  password: string;
+}
+
+interface Student extends IStudent {
+  tokens: { token: string }[];
+  verified_email: string;
+}
+
+interface StudentDocument extends Document, Student {
+  generateToken(): Promise<string>;
+}
+
+const StudentSchema = new Schema<StudentDocument>(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      maxlength: 30,
+      minlength: 2,
+      trim: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      maxlength: 30,
+      minlength: 2,
+      trim: true,
+    },
+    username: {
+      type: String,
+      maxlength: 20,
+      minlength: 2,
+      trim: true,
+      unique: true,
+    },
+    department: {
+      type: String,
+      required: true,
+      maxlength: 30,
+      minlength: 2,
+      trim: true,
+    },
+    matric_no: {
+      type: String,
+      required: true,
+      maxlength: 9,
+      minlength: 9,
+      trim: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      maxlength: 30,
+      minlength: 2,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    verified_email: {
+      type: Boolean,
+      default: false,
+    },
+    otp: String,
+    expire_at: {
+      type: Date,
+      default: Date.now,
+      expires: process.env.DELETE_UNVERIFIED_ACCOUNT_TIME,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+StudentSchema.method("generateToken", async function (this: StudentDocument) {
+  const token = jwt.sign({ id: this._id }, process.env.JWT_KEY);
+  this.tokens.push({ token });
+  await this.save();
+  return token;
+});
+
+StudentSchema.pre(
+  "save",
+  async function (this: StudentDocument, next: HookNextFunction) {
+    if (this.isModified("password")) {
+      this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+  }
+);
+
+export default model<Student>("Student", StudentSchema);
