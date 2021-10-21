b676ddc88b40e34d6a067cd9912de5941044922a
Emekarr <aceanyanwu02@gmail.com>
2021-10-21 15:38:25 +0100

    implemented error middelware and built crrate student class

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 
 import router from "./routes/index.js";
+import error_middleware from "./middleware/error_middleware.js";
 
 class App {
   private express: Application;
@@ -12,6 +13,8 @@ class App {
     this.express.get("/health", (req: Request, res: Response) => {
       res.send("server is alive");
     });
+
+    this.express.use(error_middleware);
   }
 
   listen(port: string, cb: () => void) {
--- src/controller/student_controller.ts
+++ src/controller/student_controller.ts
@@ -1,15 +1,22 @@
 import { Request, Response } from "express";
 
 import Student, { IStudent } from "../model/student";
+import CustomError from "../utils/custom_error.js";
+import ServerResponse from "../utils/response.js";
 
-const sign_up = async (req: Request, res: Response) => {
+const sign_up = async (req: Request, res: Response, next) => {
   try {
     const student_data: IStudent = req.body;
     const new_student = new Student(student_data);
     const student = await new_student.save();
-    if (!student) throw new Error("Could not create student.");
-    res.send("student created successfully.");
-  } catch (e) {}
+    if (!student) throw new CustomError("Could not create new entry.", 400);
+    new ServerResponse("Student created successfully.")
+      .data(student)
+      .statusCode(200)
+      .respond(res);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
