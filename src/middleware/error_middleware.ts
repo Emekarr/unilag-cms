f6536df777ea6d3a696afcfaffd74bc3d9691d2c
Emekarr <aceanyanwu02@gmail.com>
2021-10-21 15:35:18 +0100

    built a builder for the response class

--- src/middleware/error_middleware.ts
+++ src/middleware/error_middleware.ts
@@ -15,13 +15,13 @@ export default (
   console.log(`ERROR MESSAGE: ${err.message}\n ERROR_NAME: ${err.name}`);
   console.log(err);
   if (err.name === "CustomError") {
-    new ServerResponse(err.message).respond(
-      res,
-      (err as CustomError).error_code
-    );
+    new ServerResponse(err.message)
+      .success(false)
+      .statusCode((err as CustomError).error_code)
+      .respond(res);
   } else if (err_names.includes(err.name)) {
-    new ServerResponse(err.message).respond(res, 400);
+    new ServerResponse(err.message).success(false).statusCode(400).respond(res);
   } else {
-    new ServerResponse(err.message).respond(res, 500);
+    new ServerResponse(err.message).success(false).statusCode(500).respond(res);
   }
 };
--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,9 +1,13 @@
 import { Response } from "express";
 
-export default class ServerResponse {
+class ServerResponse {
   readonly payload: { message: string; data: object; success: boolean };
 
-  constructor(message: string, data: object = null, success: boolean = false) {
+  constructor(
+    public message: string,
+    public data: object = null,
+    public success: boolean = true
+  ) {
     this.payload = {
       message: message
         .split(" ")
@@ -16,5 +20,33 @@ export default class ServerResponse {
 
   respond(res: Response, status_code: number) {
     res.status(status_code).json(this.payload);
+  }
+}
+
+export default class ServerResponseBuilder {
+  private response: ServerResponse;
+  private status_code: number;
+
+  constructor(private message: string) {
+    this.response = new ServerResponse(message);
+  }
+
+  data(data: object) {
+    this.response.data = data;
+    return this;
+  }
+
+  success(success: boolean) {
+    this.response.success = success;
+    return this;
+  }
+
+  statusCode(status_code: number) {
+    this.status_code = status_code;
+    return this;
+  }
+
+  respond(res: Response) {
+    this.response.respond(res, this.status_code);
   }
 }
