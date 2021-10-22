3b66b93b637f8851f401687380c8d6bb53d47e36
Emekarr <aceanyanwu02@gmail.com>
2021-10-22 22:47:09 +0100

    fixed bug in route

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -5,6 +5,6 @@ import student_routes from "./student_routes";
 const router = Router();
 
 // user auth routes
-router.post("/student", student_routes);
+router.use("/student", student_routes);
 
 export default router;
